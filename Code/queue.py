9d12031d6064c8b39377f4be2e867ef7c37a9633
Anjelica <ambrosio.anjelica@gmail.com>
2020-02-12 07:59:02 -0800

    completed Queue

--- Code/queue.py
+++ Code/queue.py
@@ -22,26 +22,41 @@ class LinkedQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+         if self.length() == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Why? It only takes one process to add to a list. [TODO]"""
         # TODO: Insert given item
+          self.list.append(item)
+
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list.head.data
+
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return front item, if any
+         if self.is_empty():
+            raise ValueError("Sorry, Queue is empty!")
+        data = self.list.head.data
+        self.list.delete(self.front())
+        return data
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
@@ -63,26 +78,40 @@ class ArrayQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+         if self.length() == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Why? It only runs one process to add to a list.[TODO]"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list[0]
+
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(n) – Why? When you pop something, it has to move the indexes.[TODO]"""
         # TODO: Remove and return front item, if any
+         if self.is_empty():
+            raise ValueError("Queue is empty")
+        data = self.list[0]
+        self.list.pop(0)
+        return data
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
